93a2e004d7f24b0b9e08f3f82cc83d40e8589f5c
robua221 <robinalexander20@gmail.com>
2021-07-15 23:28:30 +0530

    breadcrumd added

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Footer } from './partials/Footer.comp'
-import { Header } from './partials/Header.comp'
+import React from "react";
+import { Footer } from "./partials/Footer.comp";
+import { Header } from "./partials/Header.comp";
 
-export const DefaultLayout = ({children}) => {
-    return (
-      
-            <div className="default-layout">
-                <div className="header"><Header/></div>
-         <main className="main">{children}</main>
-    <footer className="footer"><Footer/></footer>
-
-            </div>
-    )
-        
-}
+export const DefaultLayout = ({ children }) => {
+  return (
+    <div className="default-layout">
+      <header className="header mb-12">
+        <Header/>
+      </header>
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
