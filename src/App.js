77621aa67a17cb61c79a089db8469ab21dca082a
robua221 <robinalexander20@gmail.com>
2021-07-15 23:09:19 +0530

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/entry/dashboard/Dashboard.page';
 
 import{Entry } from "./pages/entry/Entry.page";
 
@@ -10,7 +11,7 @@ function App() {
   
 {/* <Entry/> */}
 <DefaultLayout>
-  //Dashboard
+<Dashboard/>
   </DefaultLayout>  
 
     </div>
---
+++ src/pages/entry/dashboard/Dashboard.page.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { TicketTable } from "../../../components/ticket-table/TicketTable.comp";
+import tickets from "../../../assets/data/dummy-tickets.json";
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center mt-5 mb-2">
+          <Button
+            varient="info"
+            style={{ fontSize: "2rem", padding: "10px 30px" }}
+          >
+            Add New Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center mt-5 mb-2">
+          <div>Total Ticket :50</div>
+          <div>Pending Ticket :50</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className=" mt-5">Recently added tickets</Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col className=" recent-ticket">
+          <TicketTable tickets={tickets} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
